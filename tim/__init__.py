63fd4d75b90f61a8ca98c0d6ec142a38ea418248
sahilshikalgarr <78219327+sahilshikalgarr@users.noreply.github.com>
2025-11-26 15:58:18 -0500

    Create TIM package

---
+++ tim/__init__.py
@@ -0,0 +1,11 @@
+"""
+TIM: Two-Stage Interpretable Matching
+A Python library for causal inference using interpretable matching methods.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Sahil"
+
+from .matcher import TIMatcher
+
+__all__ = ['TIMatcher']
